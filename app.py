10f0f6ef4d443ddf63a31ae5d7ee71eb72a8c4b8
hiren <hirenvadher954@gmail.com>
2022-03-23 23:27:26 +0530

    api modified

--- app.py
+++ app.py
@@ -2,23 +2,16 @@ __author__ = "Hiren Vadher"
 __version__ = "1.0.1"
 __email__ = "hirenvadher954@gmail.com"
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify
 from datetime import date
 import requests
+from flask_cors import CORS
+import json
 
 app = Flask(__name__)
+CORS(app, resources={r"*": {"origins": "*"}})
 
 paper = {}
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/BE')
-def result():
-    return render_template('result.html')
 
 
 @app.route('/<field>/<subject_code>')
@@ -26,13 +19,20 @@ def find_valid_paper(field, subject_code):
     for year in range(2016, date.today().year):
         get_paper(field, subject_code, year, "S")
         get_paper(field, subject_code, year, "W")
-    return jsonify(paper)
+    array = [{'year': i, 'paperLink': paper[i]} for i in paper]
+    return json.dumps(array)
+
+
+def get_full_name(summer_or_winter):
+    if summer_or_winter == "S":
+        return "Summer"
+    return "Winter"
 
 
 def get_paper(field, subject_code, year, summer_or_winter):
     paper_link = f"https://www.gtu.ac.in/uploads/{summer_or_winter}{year}/{field}/{subject_code}.pdf"
     if requests.get(paper_link).status_code == 200:
-        paper[str(year) + summer_or_winter] = paper_link
+        paper[str(year) + "-" + get_full_name(summer_or_winter)] = paper_link
 
 
 @app.after_request
